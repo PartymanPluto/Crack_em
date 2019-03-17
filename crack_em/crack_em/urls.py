062334bb94cbac8eb5eb38e07a8623a4508fdaba
Daniel McQuaid <2310911m@student.gla.ac.uk>
2019-03-17 20:02:40 +0000

    Basic views done, cookies not implemented, get_eggs template tag added, crack_app/urls.py updated

--- crack_em/crack_em/urls.py
+++ crack_em/crack_em/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rango/', include('rango.urls')),
+    url(r'^crack_em/', include('crack_em.urls')),
     url(r'^$', views.home, name= 'home'),
     ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
